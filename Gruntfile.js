74446d98c29e044528a3362c5758f1e72966848f
Vadim Macagon <vadim.macagon@gmail.com>
2015-10-05 14:31:53 +0700

    Add a Grunt task to build the dependencies
    This makes it easier to work on all the packages at once.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,38 @@ module.exports = function(grunt) {
       // the default target will run tsc in the directory the Gruntfile.js is in,
       // and tsc will find the tsconfig.json in the project root, so there's no
       // need to specify a project path
-      default: {}
+      'atom-package': {
+      },
+      'core-components': {
+        options: {
+          project: './node_modules/@debug-workbench/core-components/src'
+        }
+      },
+      'core-components-tests': {
+        options: {
+          project: './node_modules/@debug-workbench/core-components/test'
+        }
+      },
+      'debug-engine': {
+        options: {
+          project: './node_modules/@debug-workbench/core-components/node_modules/debug-engine/src'
+        }
+      },
+      'gdb-mi-debug-engine': {
+        options: {
+          project: './node_modules/@debug-workbench/core-components/node_modules/gdb-mi-debug-engine/src'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-tsc');
-  
-  grunt.registerTask('default', ['tsc']);
+
+  grunt.registerTask('build', [
+    'tsc:debug-engine',
+    'tsc:gdb-mi-debug-engine',
+    'tsc:core-components',
+    'tsc:atom-package'
+  ]);
+  grunt.registerTask('default', ['tsc:atom-package']);
 };
